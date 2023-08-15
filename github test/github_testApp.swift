ec6681e339e5e440e0cff7d24bf93e39b35ecb8a
Ami deCoteau <quilllune@gmail.com>
2023-08-15 13:02:24 -0400

    Initial Commit

---
+++ github test/github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_testApp.swift
+//  github test
+//
+//  Created by Ami deCoteau on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
